a7a94eb4f4b071035dcdddd99cd3dba64f0bda13
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro-2.local>
2023-11-03 14:08:44 +0630

    update

--- events/join.js
+++ events/join.js
@@ -5,12 +5,12 @@ const updateState = require("../utils/update-state");
 module.exports = (socket) => (data) => {
   const { token, room } = data;
   if (TOKEN_VERIFICATION_WEB_HOOK) {
-    const valid = isTokenValid(token);
-    if (valid) {
-      socket.join(valid.room);
-      socketid_map[socket.id] = valid.room;
-      console.log(`User joined to room ${valid.room}`);
-      updateState("join", { room: valid.room });
+    const room = isTokenValid(token);
+    if (room) {
+      socket.join(room);
+      socketid_map[socket.id] = room;
+      console.log(`User joined to room ${room}`);
+      updateState("join", { room });
     } else {
       socket.disconnect();
     }
