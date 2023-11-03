2e81abf09b47a2dd7f2ff7add2c55c9e62839411
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro-2.local>
2023-11-03 14:18:42 +0630

    update

--- utils/auth.js
+++ utils/auth.js
@@ -4,6 +4,9 @@ const { TOKEN_VERIFICATION_WEB_HOOK } = require("../constants");
 exports.isTokenValid = async (token) => {
   try {
     const res = await axios.post(`${TOKEN_VERIFICATION_WEB_HOOK}`, { token });
+    console.log(
+      `TOKEN_VERIFICATION_WEB_HOOK response: ${JSON.stringify(res.data)}`
+    );
     if (res.code == 200) {
       return res.data.data.room;
     }
